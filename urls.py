fdaebdef4044b9c46f1c8989a6397de99de73590
Dan Damian <dandamian@gmail.com>
2012-10-13 18:53:49 +0300

    Added twitter bootstrap.

--- urls.py
+++ urls.py
@@ -10,4 +10,6 @@ urlpatterns = patterns('',
     url(r'^user/login/', 'account.views.login'),
     url(r'^logout/?$',  'django.contrib.auth.views.logout_then_login'),
     url(r'^user/register/$',  'account.views.register'),
+    url(r'^discussion/', include('discussion.urls')),
+    url(r'^notifications/', include('notification.urls')),
 )
