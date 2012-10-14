1b2079a0f5ef3d97f5f6354489e2c3e7fbb57209
Dan Damian <dandamian@gmail.com>
2012-10-14 08:59:09 +0300

    Changed URL.

--- urls.py
+++ urls.py
@@ -9,7 +9,7 @@ import settings
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^user/login/', 'account.views.login'),
+    url(r'^accounts/login/', 'account.views.login'),
     url(r'^logout/?$',  'django.contrib.auth.views.logout_then_login'),
     url(r'^user/register/$',  'account.views.register'),
     url(r'^discussion/', include('discussion.urls')),
