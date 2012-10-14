c9e6e92ff1d6d95079cda95ec73b63f6fdd7a945
Vlad Temian <vladtemian@gmail.com>
2012-10-14 14:38:48 +0300

    parallax

--- urls.py
+++ urls.py
@@ -2,13 +2,14 @@ from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.views.generic import RedirectView
 from common.views import HomeView
 
 admin.autodiscover()
 import settings
 
 urlpatterns = patterns('',
-    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^$', RedirectView.as_view(url='discussion/discussions/default/inbox'), name='home'),
     url(r'^accounts/login/', 'account.views.login'),
     url(r'^accounts/logout/?$',  'django.contrib.auth.views.logout_then_login'),
     url(r'^user/register/$',  'account.views.register'),
